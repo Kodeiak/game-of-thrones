d170c2b4d85d5f69d6f61b76d7da65bf1171b741
Paul Wandrey <88623843+Kodeiak@users.noreply.github.com>
2022-04-04 09:34:41 -0500

    add category buttons via API

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,34 @@
 let apiUrl = "https://www.anapioficeandfire.com/api/";
+let categoryList = [];
 
 function loadData() {
   return fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
-      console.log(data)
-    });
-  
+      Object.keys(data).forEach(category => {
+        add(category);
+      });
+    }) .catch(e => console.error(e));
 }
 
-loadData();
+function add(category) {
+  return categoryList.push(category);
+}
+
+// 1. For each got category, create button/tab
+function addListItem(category) {
+  let gotMenu = document.querySelector(".got-menu");
+  gotMenu.innerHTML = "";
+  
+  let gotMenuItem = document.createElement("li");
+  gotMenuItem.innerText = category;
+  gotMenu.appendChild(gotMenuItem);
+
+  //event listener
+
+}
+
+categoryList.forEach(category => addListItem(category));
+
+// 2. on category click, show first 50 items as buttons
+// 3. on item click, show details

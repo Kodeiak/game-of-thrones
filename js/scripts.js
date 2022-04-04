25fb02ac25ab3e332f0502cd79125718fe2fb02d
Paul Wandrey <88623843+Kodeiak@users.noreply.github.com>
2022-04-04 11:01:59 -0500

    fetch data corresponding to category button clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,74 @@
+const gotMenu = document.querySelector(".got-menu");
+
 let apiUrl = "https://www.anapioficeandfire.com/api/";
 let categoryList = [];
+let categoryDataList = [];
 
-function loadData() {
+function addToList(list, item) {
+  return list.push(item);
+}
+
+function loadCategories() {
   return fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
-      Object.keys(data).forEach(category => {
-        add(category);
+      Object.keys(data).forEach(key => {
+        let category = {
+          name: key,
+          url: data[key]
+        };
+        addToList(categoryList, category);
       });
     }) .catch(e => console.error(e));
 }
 
-function add(category) {
-  return categoryList.push(category);
+function loadCategoryData(categoryUrl) {
+  return fetch(categoryUrl)
+    .then(response => response.json())
+    .then (data => {
+      console.log(data);
+      // addToList(categoryDataList, x)
+    })
+    .catch(e => console.error(e));    
 }
 
 // 1. For each got category, create button/tab
-function addListItem(category) {
-  let gotMenu = document.querySelector(".got-menu");
-  gotMenu.innerHTML = "";
-  
+function addListItem(item) {
+  //create list item
   let gotMenuItem = document.createElement("li");
-  gotMenuItem.innerText = category;
+  gotMenuItem.classList.add("got-menu_item");
+  
+  //create button
+  let button = document.createElement("button");
+  button.innerText = item.name;
+  button.classList.add("got-menu_item_button");
+  button.setAttribute("type", "button");
+  
+  //add elements to DOM
+  gotMenuItem.appendChild(button);
   gotMenu.appendChild(gotMenuItem);
 
   //event listener
-
+  button.addEventListener("click", function() {loadCategoryData(item.url);});
 }
 
-categoryList.forEach(category => addListItem(category));
+// function loadList(list) {
+//   loadData()
+//   .then(function() {
+//     list.forEach(function(item) {
+//       addListItem(item);
+//     });
+//   });
+// }
+
+// must be asynchronous in order to wait for data to load before creating buttons
+loadCategories()
+  .then(function() {
+    categoryList.forEach(function(category) {
+      addListItem(category);
+    });
+  });
+
 
 // 2. on category click, show first 50 items as buttons
 // 3. on item click, show details
